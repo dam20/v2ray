e71d3e46e758d8c3df021161c27d87d6c92a9490
dam20 <46676174+dam20@users.noreply.github.com>
2021-03-11 09:04:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -981,8 +981,7 @@ menu() {
     echo -e "${Green}15.${Font} update certificate crontab scheduled task"
     echo -e "${Green}16.${Font} empty certificate legacy files"
     echo -e "${Green}17.${Font} exit \n"
-    echo -e "Please enter a number: "
-    read -rp menu_num
+    echo -e "Please enter a number: " read -rp "" menu_num
     case $menu_num in
     0)
         update_sh
